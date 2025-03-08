0c736f671e00579ae6e3fcd241edf8fba6293098
Lesley Ngcobo <s225171406@mandela.ac.za>
2025-03-08 18:09:18 +0200

    Added Components

--- PropertyQuest/propertyquest.client/src/app/app.module.ts
+++ PropertyQuest/propertyquest.client/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BuyComponent } from './buy/buy.component';
+import { RentComponent } from './rent/rent.component';
+import { SellComponent } from './sell/sell.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BuyComponent,
+    RentComponent,
+    SellComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
---
+++ PropertyQuest/propertyquest.client/src/app/rent/rent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rent',
+  templateUrl: './rent.component.html',
+  styleUrl: './rent.component.css'
+})
+export class RentComponent {
+
+}
