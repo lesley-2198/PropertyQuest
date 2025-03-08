2fde08f119969fc5c4b395867c2096b09596490c
Lesley Ngcobo <s225171406@mandela.ac.za>
2025-03-08 19:02:00 +0200

    Fixed routing bug

--- PropertyQuest/propertyquest.client/src/app/app-routing.module.ts
+++ PropertyQuest/propertyquest.client/src/app/app-routing.module.ts
@@ -7,9 +7,9 @@ import { SellComponent } from './sell/sell.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: '', component: BuyComponent },
-  { path: '', component: RentComponent },
-  { path: '', component: SellComponent }
+  { path: 'buy', component: BuyComponent },
+  { path: 'rent', component: RentComponent },
+  { path: 'sell', component: SellComponent }
 ];
 
 @NgModule({
