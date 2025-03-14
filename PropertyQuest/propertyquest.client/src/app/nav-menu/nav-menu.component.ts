669efd469b314b088057332b53c1257cc56ae63a
Lesley Ngcobo <s225171406@mandela.ac.za>
2025-03-15 01:29:10 +0200

    Made some responsive changes to the interface
    Home: Made the font-size in the cards to change along with the size of the cards.
    Nav-menu: Made it so that when the screen gets small enough, the buttons disappear and a single menu button (with the other buttons ) appears.

--- PropertyQuest/propertyquest.client/src/app/nav-menu/nav-menu.component.ts
+++ PropertyQuest/propertyquest.client/src/app/nav-menu/nav-menu.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrl: './nav-menu.component.scss'
+  styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
+  isSmallScreen: boolean = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe('(max-width: 768px)').subscribe(result => {
+      this.isSmallScreen = result.matches;
+    });
+  }
 }
